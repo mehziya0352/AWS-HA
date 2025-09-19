aa9fdf75c034c27ef31e5acb1c5b97b65e1c483a
mehziya0352 <mehziya0352@gmail.com>
2025-09-19 15:58:54 +0530

    Update launch_template.tf

--- terraform/launch_template.tf
+++ terraform/launch_template.tf
@@ -1,6 +1,6 @@
 resource "aws_launch_template" "app_lt" {
   name   = "${var.project}-lt"
-  image_id      = aws_ami_from_instance.from_instance.ami
+  image_id      = aws_ami_from_instance.from_instance.id
   instance_type = "t2.micro"
   key_name      = var.key_name  
   vpc_security_group_ids = [aws_security_group.app_sg.id]
