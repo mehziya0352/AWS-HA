0e232bbe23da92e2e4d2559917558f80a46e4f8b
mehziya0352 <mehziya0352@gmail.com>
2025-09-19 19:24:20 +0530

    Update userdata.sh

--- terraform/userdata.sh
+++ terraform/userdata.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-# Update & install dependencies
 apt-get update -y
-apt-get install -y wget unzip curl awscli
+apt-get install -y wget unzip curl
 
-# Download CloudWatch Agent
+# Download & install CloudWatch Agent
 wget https://amazoncloudwatch-agent.s3.amazonaws.com/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
 dpkg -i amazon-cloudwatch-agent.deb
 
 # Download CloudWatch config from S3
-aws s3 cp s3://mybucket/cloudwatch-config.json /opt/aws/amazon-cloudwatch-agent/config.json
+aws s3 cp s3://my-json-bkt123/cloudwatch-config.json /opt/aws/amazon-cloudwatch-agent/config.json
 
 # Start CloudWatch Agent
 /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
