b7d1e3710fa4f06ceef8a0cfebd421ea8b79c6c7
mehziya0352 <mehziya0352@gmail.com>
2025-09-19 11:26:51 +0530

    Update launch_template.tf

--- terraform/launch_template.tf
+++ terraform/launch_template.tf
@@ -4,18 +4,63 @@
   instance_type = "t2.micro"
   key_name      = var.key_name  
   vpc_security_group_ids = [aws_security_group.app_sg.id]
+  user_data = base64encode(<<-EOT
+    #!/bin/bash
+    # Update and install required packages
+    apt-get update -y
+    apt-get install -y wget unzip curl
 
-  user_data = base64encode(<<EOF
-#!/bin/bash
-# Log output
-exec > /var/log/user-data.log 2>&1
-set -x
-# Download and run the script from S3
-aws s3 cp s3://cloudwatch-script-bkt/userdata.sh /tmp/userdata.sh
-chmod +x /tmp/userdata.sh
-bash /tmp/userdata.sh
-EOF
-)
+    # Download and install CloudWatch agent
+    wget https://amazoncloudwatch-agent.s3.amazonaws.com/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
+    dpkg -i amazon-cloudwatch-agent.deb
+
+    # Create CloudWatch Agent config
+    cat <<'EOF' > /opt/aws/amazon-cloudwatch-agent/bin/config.json
+    {
+      "agent": {
+        "metrics_collection_interval": 60,
+        "run_as_user": "root"
+      },
+      "metrics": {
+        "append_dimensions": {
+          "AutoScalingGroupName": "$${aws:AutoScalingGroupName}",
+          "InstanceId": "$${aws:InstanceId}"
+        },
+        "metrics_collected": {
+          "cpu": {
+            "measurement": [
+              "cpu_usage_idle",
+              "cpu_usage_user",
+              "cpu_usage_system"
+            ],
+            "metrics_collection_interval": 60,
+            "resources": ["*"]
+          },
+          "mem": {
+            "measurement": [
+              "mem_used_percent"
+            ],
+            "metrics_collection_interval": 60
+          },
+          "disk": {
+            "measurement": [
+              "disk_used_percent"
+            ],
+            "metrics_collection_interval": 60,
+            "resources": ["/"]
+          }
+        }
+      }
+    }
+    EOF
+
+    # Start CloudWatch agent
+    /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+      -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/bin/config.json -s
+  EOT
+  )
+}
+
 
 
   iam_instance_profile {
