11fdd5c5dddc665e50f6f32faa0e7749f795c2d6
mehziya0352 <mehziya0352@gmail.com>
2025-09-20 21:05:40 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,7 @@
 output "alb_dns" { value = aws_lb.alb.dns_name }
 output "asg_name" { value = aws_autoscaling_group.asg.name }
+output "asg_instance_ids" {
+  value = aws_autoscaling_group.asg.instances
+}
 output "ami_id"   { value = aws_ami_from_instance.from_instance.id }
 output "vpc_id"   { value = aws_vpc.this.id }
