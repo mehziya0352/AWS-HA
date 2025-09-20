5212e27ba55e473a8ee62b67bddaded563280f37
mehziya0352 <mehziya0352@gmail.com>
2025-09-20 12:05:28 +0530

    Update iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,5 +1,5 @@
 # -----------------------------
-# Managed Policies (AWS provided)
+# Managed Policies
 # -----------------------------
 data "aws_iam_policy" "cw_agent_policy" {
   arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
@@ -28,29 +28,7 @@ data "aws_iam_policy_document" "ec2_assume_role" {
 resource "aws_iam_role" "ec2_role" {
   name               = "${var.project}-ec2-role"
   assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
-
-  tags = {
-    Name = "${var.project}-ec2-role"
-  }
-}
-
-# -----------------------------
-# Custom S3 Read Access (inline policy)
-# -----------------------------
-resource "aws_iam_role_policy" "ec2_s3_read" {
-  name = "${var.project}-ec2-s3-read"
-  role = aws_iam_role.ec2_role.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect   = "Allow"
-        Action   = ["s3:GetObject"]
-        Resource = "arn:aws:s3:::cloudwatch-script-bkt/*"
-      }
-    ]
-  })
+  tags = { Name = "${var.project}-ec2-role" }
 }
 
 # -----------------------------
@@ -64,6 +42,44 @@ resource "aws_iam_role_policy_attachment" "attach_cw" {
 resource "aws_iam_role_policy_attachment" "attach_ssm" {
   role       = aws_iam_role.ec2_role.name
   policy_arn = data.aws_iam_policy.ssm_policy.arn
+}
+
+# -----------------------------
+# S3 Read Access Policy for EC2
+# -----------------------------
+data "aws_iam_policy_document" "s3_read_access" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["arn:aws:s3:::cloudwatch-script-bkt/*"]
+    effect    = "Allow"
+  }
+}
+
+resource "aws_iam_role_policy" "ec2_s3_read" {
+  name   = "${var.project}-ec2-s3-read"
+  role   = aws_iam_role.ec2_role.name
+  policy = data.aws_iam_policy_document.s3_read_access.json
+}
+
+# -----------------------------
+# PassRole Policy
+# -----------------------------
+data "aws_iam_policy_document" "passrole_doc" {
+  statement {
+    effect    = "Allow"
+    actions   = ["iam:PassRole"]
+    resources = [aws_iam_role.ec2_role.arn]
+  }
+}
+
+resource "aws_iam_policy" "passrole_policy" {
+  name   = "${var.project}-passrole-permission"
+  policy = data.aws_iam_policy_document.passrole_doc.json
+}
+
+resource "aws_iam_role_policy_attachment" "passrole_attach" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = aws_iam_policy.passrole_policy.arn
 }
 
 # -----------------------------
@@ -71,83 +87,5 @@ resource "aws_iam_role_policy_attachment" "attach_ssm" {
 # -----------------------------
 resource "aws_iam_instance_profile" "ec2_profile" {
   name = "${var.project}-instance-profile"
-  role = aws_iam_role.ec2_role.name
-}
-# -----------------------------
-# Managed Policies
-# -----------------------------
-data "aws_iam_policy" "cw_agent_policy" {
-  arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
-}
-
-data "aws_iam_policy" "ssm_policy" {
-  arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-}
-
-# -----------------------------
-# EC2 Assume Role Policy
-# -----------------------------
-data "aws_iam_policy_document" "ec2_assume_role" {
-  statement {
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
-    }
-  }
-}
-
-# -----------------------------
-# EC2 Role
-# -----------------------------
-resource "aws_iam_role" "ec2_role" {
-  name               = "${var.project}-role"
-  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
-
-  tags = {
-    Name = "${var.project}-role"
-  }
-}
-
-# -----------------------------
-# Attach Managed Policies
-# -----------------------------
-resource "aws_iam_role_policy_attachment" "attach_cw" {
-  role       = aws_iam_role.ec2_role.name
-  policy_arn = data.aws_iam_policy.cw_agent_policy.arn
-}
-
-resource "aws_iam_role_policy_attachment" "attach_ssm" {
-  role       = aws_iam_role.ec2_role.name
-  policy_arn = data.aws_iam_policy.ssm_policy.arn
-}
-
-# -----------------------------
-# Custom S3 Read Access Policy
-# -----------------------------
-resource "aws_iam_policy" "s3_read" {
-  name = "${var.project}-s3-read"
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect   = "Allow"
-        Action   = ["s3:GetObject"]
-        Resource = "arn:aws:s3:::cloudwatch-script-bkt/*"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "attach_s3" {
-  role       = aws_iam_role.ec2_role.name
-  policy_arn = aws_iam_policy.s3_read.arn
-}
-
-# -----------------------------
-# Instance Profile
-# -----------------------------
-resource "aws_iam_instance_profile" "ec2_profile" {
-  name = "${var.project}-ip"   # keep short to avoid 255 char bug
   role = aws_iam_role.ec2_role.name
 }
