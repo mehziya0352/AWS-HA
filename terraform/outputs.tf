8851375e6339f0ab5c56e1920cd4b1e318777b1a
mehziya0352 <mehziya0352@gmail.com>
2025-09-20 21:08:06 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,13 @@
 output "alb_dns" { value = aws_lb.alb.dns_name }
 output "asg_name" { value = aws_autoscaling_group.asg.name }
-output "asg_instance_ids" {
-  value = aws_autoscaling_group.asg.instances
+
+data "aws_autoscaling_group" "asg_data" {
+  name = aws_autoscaling_group.asg.name
 }
-output "ami_id"   { value = aws_ami_from_instance.from_instance.id }
-output "vpc_id"   { value = aws_vpc.this.id }
+
+output "asg_instance_ids" {
+  value = data.aws_autoscaling_group.asg_data.instances[*].id
+}
+
+output "ami_id" { value = aws_ami_from_instance.from_instance.id }
+output "vpc_id" { value = aws_vpc.this.id }
