527e7e0f2301b59770e40fd556a7a317cab2a0e8
mehziya0352 <mehziya0352@gmail.com>
2025-09-20 16:29:18 +0530

    Update launch_template.tf

--- terraform/launch_template.tf
+++ terraform/launch_template.tf
@@ -2,7 +2,6 @@ resource "aws_launch_template" "app_lt" {                                       
   update_default_version = true
   image_id      = aws_ami_from_instance.from_instance.id
   instance_type = "t2.micro"
-  key_name      = var.key_name
   vpc_security_group_ids = [aws_security_group.app_sg.id]
   iam_instance_profile {
     name = aws_iam_instance_profile.ec2_profile.name
